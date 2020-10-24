79fdb6a87fcdc54bb6d4b8fefad5b8f2625e3986
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-10-24 17:07:20 +0300

    finished the route (its gilad)

--- backend/remp.ts
+++ backend/remp.ts
@@ -3,42 +3,58 @@
 import express from "express";
 import { getTransactionsForUserForApi } from "./database";
 import { ensureAuthenticated, validateMiddleware } from "./helpers";
-import { sanitizeTransactionStatus, sanitizeRequestStatus, isTransactionQSValidator, } from "./validators";
-import { getPaginatedItems } from "../src/utils/transactionUtils";
+import {
+  sanitizeTransactionStatus,
+  sanitizeRequestStatus,
+  isTransactionQSValidator,
+} from "./validators";
+import {
+  getPaginatedItems,
+  isAcceptedRequestTransaction,
+  isRequestTransaction,
+} from "../src/utils/transactionUtils";
+import { Transaction } from "models";
 const router = express.Router();
 
 router.get(
-    "/",
-    ensureAuthenticated,
-    validateMiddleware([
-        sanitizeTransactionStatus,
-        sanitizeRequestStatus,
-        ...isTransactionQSValidator,
-    ]),
-    (req, res) => {
-        /* istanbul ignore next */
-        const transactions = getTransactionsForUserForApi(req.user?.id!, req.query);
+  "/",
+  ensureAuthenticated,
+  validateMiddleware([
+    sanitizeTransactionStatus,
+    sanitizeRequestStatus,
+    ...isTransactionQSValidator,
+  ]),
+  (req, res) => {
+    /* istanbul ignore next */
+    const transactions = getTransactionsForUserForApi(req.user?.id!, req.query) as Transaction[];
+    let functionsData: Transaction[] = [];//if i am charged, the ammount is minus, if payed the ammount is plus
+    transactions.forEach((transaction) => {
+      if (transaction.receiverId == req.user?.id) {
+        if (isRequestTransaction(transaction)) {
+          if (isAcceptedRequestTransaction(transaction)) {
+            transaction.amount *= -1;
+            functionsData.push(transaction);
+          } //got charged, invert ammount!
+        } else functionsData.push(transaction); // i got paid, ammount should stay
+      } else if (transaction.senderId == req.user?.id) {
+        if (isRequestTransaction(transaction)) {
+          if (isAcceptedRequestTransaction(transaction)) functionsData.push(transaction); //i got paid, ammount should stay
+        } else {
+          transaction.amount *= -1;
+          functionsData.push(transaction);
+        } //got charged, invert ammount!
+      }
+    });
 
-        // const { totalPages, data: paginatedItems } = getPaginatedItems(
-        //     req.query.page,
-        //     req.query.limit,
-        //     transactions
-        // );
+    // const sentData={
+    //     Pie:galsPiefunction(functionsData),
+    //     Column:galsColumnFunction(functionsData)
+    // }
 
-        // res.status(200);
-        // res.json({
-        //     pageData: {
-        //         page: res.locals.paginate.page,
-        //         limit: res.locals.paginate.limit,
-        //         hasNextPages: res.locals.paginate.hasNextPages(totalPages),
-        //         totalPages,
-        //     },
-        //     results: paginatedItems,
-        // });
 
-        console.log(req.query);
-        res.json(transactions)
-    }
+    console.log(req.query);
+    res.json(functionsData);
+  }
 );
 
-export default router
+export default router;
