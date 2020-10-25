85c64fc6d94d42a0fcc4a873d523e26815df630d
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-10-25 15:41:23 +0200

    collumn chart added

--- src/components/Charts/CollumnChart.tsx
+++ src/components/Charts/CollumnChart.tsx
@@ -1,31 +1,81 @@
 import React from "react";
-
-interface ColumnsProps {
-  size: number;
-  data: Month[];
-}
+import styled from 'styled-components'
+import Monthly from "./Monthly"
 
 interface Month {
-  month: string;
-  expenses: number;
-  income: number;
+  expenses: number, income: number, date: number;
 }
 
-const ChartCollumn: React.FC<ColumnsProps> = ({ size, data }) => {
-  const maxMonth = data.reduce(
-    (a, b): Month => {
-      return {
-        month: "accumulator",
-        expenses: Math.max(a.expenses, b.expenses),
-        income: Math.max(a.income, b.income),
-      };
-    }
-  );
-  const max:number=Math.max(maxMonth.income,maxMonth.expenses)
-  
-  return <div className="grid">
-      
-  </div>;
-};
+interface  props{
+  allYear: Month[],
+  size: number
+}
 
-export default ChartCollumn;
+
+
+const ColumnTable = styled.div`
+ background-color: white;
+ width: 80%;
+ display: flex;
+ border-radius: 30px 30px 1px 1px;
+ flex-direction: row;
+  justify-content: space-around;
+`;
+
+
+const X = styled.div`
+ background-color: white;
+ width: 5%;
+ display: flex;
+ height: 100%; 
+ align-self: center;
+ flex-direction: column;
+ align-self: space-around;
+ justify-content: space-between;
+ align-self: flex-end;
+ padding-bottom: 12px;
+`;
+
+const Money = styled.div`
+ background-color: white;
+ width: 100%;
+ display: flex;
+ flex-direction: column;
+ height: 10%; 
+ align-self: flex-end;
+align-content: flex-end;
+justify-content: flex-end;
+font-size:10px;
+white-space: nowrap;
+
+`;
+
+
+const ColumnChart: React.FC<props> = ({allYear,size}) => {
+  const max: number= Math.max(...allYear.map((month) => {
+     return Math.max(month.expenses,month.income)}))
+     console.log(allYear)
+  return (
+    <ColumnTable >
+    {allYear.map((month) => (
+       <Monthly expenses={month.expenses} income={month.income} month={month.date} max={max} size={size}></Monthly>
+    ))}
+
+      <X>
+        <Money> - {10* max/8}</Money>
+        <Money> - {9 * max/8}</Money>
+        <Money> - {8 * max/8}</Money>
+        <Money> - {7 * max/8}</Money>
+        <Money> - {6 * max/8}</Money>
+        <Money> - {5 * max/8}</Money>
+        <Money> - {4 * max/8}</Money>
+        <Money> - {3* max/8}</Money>
+        <Money> - {2 * max/8}</Money>
+        <Money> - {max/8}</Money>
+        <Money> - 0</Money>
+      </X>
+    </ColumnTable>
+  );
+}
+
+export default ColumnChart;
---
+++ src/components/Charts/Monthly.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styled from 'styled-components'
+
+interface DateProps {
+  date: number;
+};
+
+interface IncomeProps {
+  income: number;
+  max: number;
+};
+
+interface ExpensesProps {
+  expenses: number;
+  max: number;
+};
+interface columnProps {
+  expenses: number, income: number, max: number, month: number, size:number;
+}
+
+const ExpensesColumn = styled.div<ExpensesProps>`
+ background-color: #ff5151;
+ width: 32%;
+ min-width:  20px;
+  display: flex;
+ color:  #ff5151;
+ align-self: flex-end;
+ border-radius:  5px 5px 1px 1px;
+ height: ${props => props.expenses / props.max * 73}%;
+`;
+
+const IncomeColumn = styled.div<IncomeProps>`
+ background-color: #65ff65;
+ width: 32%;
+ min-width:  20px;
+ color:  #65ff65;
+ display: flex;
+ align-self: flex-end;
+ border-radius:  5px 5px 1px 1px;
+  height:${props => props.income / props.max * 73}%;
+`;
+const Date = styled.div`
+ background-color: white;
+ width: 100%;
+ color:  black;
+ display: flex;
+ align-self: flex-end;
+ justify-content: center;
+`;
+
+
+const Monthly: React.FC<columnProps> = ({ expenses, income, max, month, size }) => {
+  return (
+    <div >
+      <div style={{ height: size, display: 'flex', flexDirection: 'row', justifyContent: "space-around", backgroundColor: 'white' }}>
+        <ExpensesColumn expenses={expenses} max={max}></ExpensesColumn>
+        <IncomeColumn income={income} max={max}></IncomeColumn>
+      </div>
+      <Date>{month}</Date>
+    </div>
+  );
+}
+
+export default Monthly;
