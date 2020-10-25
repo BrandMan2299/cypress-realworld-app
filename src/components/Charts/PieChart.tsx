d8dfa1d8541c341e09a0a382935387d724d98513
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-10-25 13:06:55 +0200

    should work

--- src/components/Charts/PieChart.tsx
+++ src/components/Charts/PieChart.tsx
@@ -11,7 +11,40 @@ interface PieProps{
 
 
 
+<<<<<<< HEAD
 enum Colors{blue,red,orange,green,yellow}
+=======
+const ChartPie: React.FC<PieProps> = ({ size, data }) => {
+  const canvas: React.RefObject<HTMLCanvasElement> = useRef(null);
+  // const sectors: Array<[string, Sector, string]> = [];
+  const [sectors,setSectors]=useState<sectors>([])
+<<<<<<< Updated upstream
+  const [tags,setTags]=useState([<div></div>,])
+=======
+  const [tags,setTags]=useState<JSX.Element[]>([<div></div>,])
+>>>>>>> Stashed changes
+  const [showContext,setShowContext]=useState<boolean>(false)
+  const [context,setContext]=useState<context>({top:0,left:0,text:""})
+  function draw(
+    el: HTMLCanvasElement,
+    startAngle: number,
+    precentage: number,
+    color: string
+  ): number {
+    let begining = 1.5 * Math.PI + startAngle;
+    let ctx = el.getContext("2d") as CanvasRenderingContext2D;
+    ctx.beginPath();
+    ctx.arc(
+      size / 2,
+      size / 2,
+      size / 2,
+      begining,
+      begining + ((2 * precentage) / 100) * Math.PI
+    );
+    ctx.lineTo(size / 2, size / 2);
+    ctx.fillStyle = color;
+    ctx.fill();
+>>>>>>> parent of 95d98b6... f
 
 const ChartPie:React.FC<PieProps>=({size,data})=>{
     const canvas: React.RefObject<HTMLCanvasElement>=useRef(null)
